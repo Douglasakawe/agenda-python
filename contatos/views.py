5edc0a9bd02d586dbeca68324ceff08451a73548
Douglasakawe <douglasakawe@hotmail.com>
2022-11-07 14:52:20 -0300

    first commit

---
+++ contatos/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Contato
+from django.core.paginator import Paginator
+
+def index(request):
+	contatos = Contato.objects.all()
+	paginator = Paginator(contatos, 10)
+
+	return render(request, 'contatos/index.html', {
+		'contatos': contatos
+	})
+
+	page = request.GET.get('p')
+	contatos = paginator.get_page(page)
+
+def ver_contato(request, contato_id):
+	contato = get_object_or_404(Contato, id=contato_id)
+	return render(request, 'contatos/ver_contato.html', {
+		'contato': contato
+	})
+
